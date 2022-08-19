9f83871966255ad4ce91bae10562b1e1a16debd7
HYUK LEE <ruldes76@gmail.com>
2022-08-19 17:35:41 +0900

    commit

---
+++ EFCoreCodeFirstSample/Models/Purchase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCoreCodeFirstSample.Models
+{
+	public class Purchase
+	{
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+        public int productId { get; set; }
+		public int userId { get; set; }
+		public int price { get; set; }
+		public DateTime date { get; set; }
+		public DateTime updateDate { get; set; }
+	}
+}
+
---
+++ EFCoreCodeFirstSample/Models/PurchaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EFCoreCodeFirstSample.Models
+{
+    public class PurchaseContext : DbContext
+    {
+        public PurchaseContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
+
